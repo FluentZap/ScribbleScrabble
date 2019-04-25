0d97de462d9813b650a4b66c4b4de90b716d6200
Lindsey Baker and Todd Aden <todd@>
2019-04-25 09:24:41 -0700

    Add initila files

---
+++ Scrabble.test/ScrabbleTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using ScrabbleSpace;
+
+namespace Scrabble.Tests
+{
+  [TestClass]
+  public class TestMethods
+  {
+    // [TestMethod]
+    // public void TestKeywordSave()
+    // {
+    //   Anagram anagram = new Anagram("bread");
+    //   Assert.AreEqual("bread", anagram.GetKeyWord());
+    // }
+
+  }
+}
---
+++ Scrabble/Scrabble.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace ScrabbleSpace
+{
+  public class Scrabble
+  {
+
+  }
+}
